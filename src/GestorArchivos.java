df25c4fa739541104fc1c6df2ec97f37afbd338c
Nuel2004 <manurodaspinilla@gmail.com>
2025-03-26 17:28:58 +0100

    3

--- src/GestorArchivos.java
+++ src/GestorArchivos.java
@@ -1,3 +1,5 @@
 public class GestorArchivos {
-    
+    public static void seleccionarCarpeta(){
+        
+    }
 }
--- src/Xml.java
+++ src/Xml.java
@@ -1,9 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Xml implements Archivos{
 
     @Override
     public void aTextoPlano() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'aTextoPlano'");
+        
     }
 
     @Override
