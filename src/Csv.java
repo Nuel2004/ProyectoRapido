89855e38614649bf0b7c9a21450e5fa7ea3138f3
Nuel2004 <manurodaspinilla@gmail.com>
2025-03-27 16:32:48 +0100

    1

--- src/Csv.java
+++ src/Csv.java
@@ -1,27 +1,27 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.BufferedWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class Csv extends Archivos {
-    
+public class Csv implements Archivos{
+
     @Override
-    public void leer(String path, List<Map<String, String>> datos) {
+    public void aTextoPlano(String path, List<Map<String, String>> datos) {
         System.out.println("Leyendo archivo CSV...");
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String linea = br.readLine();
             if (linea != null) {
-                String[] headers = linea.split(",");
+                String[] arrayEncabezado = linea.split(",");
                 while ((linea = br.readLine()) != null) {
                     String[] valores = linea.split(",");
                     Map<String, String> fila = new HashMap<>();
-                    for (int i = 0; i < headers.length; i++) {
-                        fila.put(headers[i], valores[i]);
+                    for (int i = 0; i < arrayEncabezado.length; i++) {
+                        fila.put(arrayEncabezado[i], valores[i]);
                     }
                     datos.add(fila);
                 }
@@ -32,14 +32,13 @@ public class Csv extends Archivos {
         }
     }
 
-
     @Override
-    public void exportar(String path, List<Map<String, String>> datos) {
-        System.out.println("Exportando a CSV...");
+    public void darFormato(String path, List<Map<String, String>> datos) {
+         System.out.println("Exportando a CSV...");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
             if (!datos.isEmpty()) {
-                Set<String> headers = datos.get(0).keySet();
-                bw.write(String.join(",", headers));
+                Set<String> arrayEncabezado = datos.get(0).keySet();
+                bw.write(String.join(",", arrayEncabezado));
                 for (Map<String, String> fila : datos) {
                     bw.write(String.join(",", fila.values()));
                 }
@@ -49,4 +48,7 @@ public class Csv extends Archivos {
             System.err.println("Error: " + e.getMessage());
         }
     }
+    
+    
+    
 }
