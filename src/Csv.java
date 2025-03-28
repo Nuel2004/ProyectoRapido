b679f222ad36c73042b6d6b3d7876dc588d00be6
Nuel2004 <manurodaspinilla@gmail.com>
2025-03-28 16:57:24 +0100

    ahora si qeu si pablo jurao

--- src/Csv.java
+++ src/Csv.java
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
-import java.io.*;
-=======
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
->>>>>>> ramadeveloper
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-<<<<<<< HEAD
-public class Csv implements Archivos{
-
-    @Override
-    public void aTextoPlano(String path, List<Map<String, String>> datos) {
-=======
 public class Csv extends Archivos{
     @Override
     public void leer(String path, List<Map<String, String>> datos) {
->>>>>>> ramadeveloper
         System.out.println("Leyendo archivo CSV...");
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String linea = br.readLine();
@@ -43,11 +32,7 @@ public class Csv extends Archivos{
     }
 
     @Override
-<<<<<<< HEAD
-    public void darFormato(String path, List<Map<String, String>> datos) {
-=======
     public void exportar(String path, List<Map<String, String>> datos) {
->>>>>>> ramadeveloper
         System.out.println("Exportando a CSV...");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
             if (!datos.isEmpty()) {
@@ -63,9 +48,6 @@ public class Csv extends Archivos{
         }
     }
     
-<<<<<<< HEAD
-=======
     
     
->>>>>>> ramadeveloper
 }
--- src/Xml.java
+++ src/Xml.java
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import java.util.List;
-import java.util.Map;
-
-public class Xml implements Archivos{
-
-    @Override
-    public void aTextoPlano(String path, List<Map<String, String>> datos) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'aTextoPlano'");
-    }
-
-    @Override
-    public void darFormato(String path, List<Map<String, String>> datos) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'darFormato'");
-    }
-    
-}
-=======
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -107,4 +87,3 @@ public class Xml extends Archivos {
                     .replace("'", "&apos;");
     }
 }
->>>>>>> ramadeveloper
